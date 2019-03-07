7a7140b774ea8a614c2b683247d71d53ab05507f
zyz <zyz@1000phone.com>
2019-03-07 11:12:25 +0800

    爱鲜蜂－首页1

--- Python181AXF/app/models.py
+++ Python181AXF/app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+# 基础类
+class BaseModel(models.Model):
+    img = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    trackid = models.CharField(max_length=10)
+
+    class Meta:
+        abstract = True
+
+# 轮播图　模型类
+# insert into axf_wheel(img,name,trackid)
+class Wheel(BaseModel):
+    class Meta:
+        db_table = 'axf_wheel'
+
+
+# 导航　模型类
+class Nav(BaseModel):
+    class Meta:
+        db_table = 'axf_nav'
+
+
+# 每日必购
+class Mustbuy(BaseModel):
+    class Meta:
+        db_table = 'axf_mustbuy'
+
+
+# 部分商品
+class Shop(BaseModel):
+    class Meta:
+        db_table = 'axf_shop'
--- Python181AXF/app/views.py
+++ Python181AXF/app/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
+from app.models import Wheel, Nav, Mustbuy, Shop
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home/home.html')
+    # 轮播图数据
+    wheels = Wheel.objects.all()
+
+    # 导航
+    navs = Nav.objects.all()
+
+    # 每日必购
+    mustbuys = Mustbuy.objects.all()
+
+    # 商品部分
+    shops = Shop.objects.all()
+    shophead = shops[0]
+    shoptabs = shops[1:3]
+    shopclass_list = shops[3:7]
+    shopcommends = shops[7:11]
+
+    response_dir = {
+        'wheels':wheels,
+        'navs': navs,
+        'mustbuys': mustbuys,
+        'shophead': shophead,
+        'shoptabs': shoptabs,
+        'shopclass_list': shopclass_list,
+        'shopcommends': shopcommends
+    }
+
+    return render(request, 'home/home.html', context=response_dir)
 
 
 def market(request):
