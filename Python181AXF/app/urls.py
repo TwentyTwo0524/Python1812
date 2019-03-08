3de014c26559279bb4549ac4b8f410366fe84460
zyz <zyz@1000phone.com>
2019-03-08 11:32:42 +0800

    闪购超市-侧边栏(分类功能完成)

--- Python181AXF/app/urls.py
+++ Python181AXF/app/urls.py
@@ -4,7 +4,11 @@ from app import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),  # 首页
-    url(r'^market/$', views.market, name='market'), # 闪购超市
+
+    # url(r'^market/$', views.market, name='marketbase'), # 闪购超市
+    # url(r'^market/(\d+)/$', views.market, name='market'), # 闪购超市
+    url(r'^market/$', views.market, name='market'),
+
     url(r'^cart/$', views.cart, name='cart'),   # 购物车
     url(r'^mine/$', views.mine, name='mine'),   # 我的
 ]
--- Python181AXF/app/views.py
+++ Python181AXF/app/views.py
@@ -1,5 +1,7 @@
+import random
+
 from django.shortcuts import render
-from app.models import Wheel, Nav, Mustbuy, Shop, Mainshow, Foodtype
+from app.models import Wheel, Nav, Mustbuy, Shop, Mainshow, Foodtype, Goods
 
 
 # Create your views here.
@@ -37,19 +39,56 @@ def home(request):
     return render(request, 'home/home.html', context=response_dir)
 
 
+
+# def market(request, categoryid=104749):
 def market(request):
     # 分类信息
     foodtypes = Foodtype.objects.all()
 
+
+    # 商品信息
+    # goods_list = Goods.objects.all()[0:5]
+    # 默认打开页面  热销榜
+    # 点击左侧分类，即显示对应分类 商品信息  【传参数categoryid】
+    # goods_list = Goods.objects.filter(categoryid=categoryid)
+
+    # 客户端 需要记录 点击的分类下标 【cookies， 会自动携带】
+    index = int(request.COOKIES.get('index', '0'))
+    # 根据index 获取 对应的 分类ID
+    categoryid = foodtypes[index].typeid
+    # 根据 分类ID 获取对应分类信息
+    goods_list = Goods.objects.filter(categoryid=categoryid)
+
+    # 获取子类信息
+    childtypenames = foodtypes[index].childtypenames
+    # 存储子类信息 列表
+    childtype_list = []
+    # 将对应的子类拆分出来
+    for item in childtypenames.split('#'):
+        # item  >> 全部分类:0
+        # item  >> 子类名称: 子类ID
+        item_arr = item.split(':')
+        temp_dir = {
+            'name': item_arr[0],
+            'id': item_arr[1]
+        }
+
+        childtype_list.append(temp_dir)
+
+
     response_dir = {
-        'foodtypes': foodtypes
+        'foodtypes': foodtypes,
+        'goods_list': goods_list,
+        'childtype_list': childtype_list
     }
+
 
     return render(request, 'market/market.html', context=response_dir)
 
 
 def cart(request):
-    return render(request, 'cart/cart.html')
+    temp = random.randrange(4,63)
+    return render(request, 'cart/cart.html', context={'temp':temp})
 
 
 def mine(request):
