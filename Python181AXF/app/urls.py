4e75732cb53142bedf40228f871aca4860579989
zyz <zyz@1000phone.com>
2019-03-08 16:49:40 +0800

    爱鲜蜂-闪购超市(完成)&我的(部分)

--- Python181AXF/app/urls.py
+++ Python181AXF/app/urls.py
@@ -5,10 +5,15 @@ from app import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),  # 首页
 
-    # url(r'^market/$', views.market, name='marketbase'), # 闪购超市
-    # url(r'^market/(\d+)/$', views.market, name='market'), # 闪购超市
-    url(r'^market/$', views.market, name='market'),
+    url(r'^market/$', views.market, name='marketbase'), # 闪购超市
+    url(r'^market/(?P<childid>\d+)/(?P<sortid>\d+)/$', views.market, name='market'), # 闪购超市
+
+    # url(r'^market/$', views.market, name='market'),
 
     url(r'^cart/$', views.cart, name='cart'),   # 购物车
     url(r'^mine/$', views.mine, name='mine'),   # 我的
+
+    url(r'^login/$', views.login, name='login'),    # 登录
+    url(r'^logout/$', views.logout, name='logout'),    # 退出
+    url(r'^register/$', views.register, name='register'),    # 登录
 ]
--- Python181AXF/app/views.py
+++ Python181AXF/app/views.py
@@ -1,7 +1,11 @@
+import hashlib
 import random
+import time
 
-from django.shortcuts import render
-from app.models import Wheel, Nav, Mustbuy, Shop, Mainshow, Foodtype, Goods
+from django.core.cache import cache
+from django.shortcuts import render, redirect
+
+from app.models import Wheel, Nav, Mustbuy, Shop, Mainshow, Foodtype, Goods, User
 
 
 # Create your views here.
@@ -41,7 +45,7 @@ def home(request):
 
 
 # def market(request, categoryid=104749):
-def market(request):
+def market(request, childid='0', sortid='0'):
     # 分类信息
     foodtypes = Foodtype.objects.all()
 
@@ -57,7 +61,22 @@ def market(request):
     # 根据index 获取 对应的 分类ID
     categoryid = foodtypes[index].typeid
     # 根据 分类ID 获取对应分类信息
-    goods_list = Goods.objects.filter(categoryid=categoryid)
+    # goods_list = Goods.objects.filter(categoryid=categoryid)
+
+    # 子类
+    if childid == '0':
+        goods_list = Goods.objects.filter(categoryid=categoryid)
+    else:
+        goods_list = Goods.objects.filter(categoryid=categoryid).filter(childcid=childid)
+
+    # 排序
+    # 0默认综合排序   1销量排序     2价格最低   3价格最高
+    if sortid == '1':
+        goods_list = goods_list.order_by('-productnum')
+    elif sortid == '2':
+        goods_list = goods_list.order_by('price')
+    elif sortid == '3':
+        goods_list = goods_list.order_by('-price')
 
     # 获取子类信息
     childtypenames = foodtypes[index].childtypenames
@@ -79,7 +98,8 @@ def market(request):
     response_dir = {
         'foodtypes': foodtypes,
         'goods_list': goods_list,
-        'childtype_list': childtype_list
+        'childtype_list': childtype_list,
+        'childid': childid
     }
 
 
@@ -92,4 +112,60 @@ def cart(request):
 
 
 def mine(request):
-    return render(request, 'mine/mine.html')
+    # 获取
+    token = request.session.get('token')
+    userid = cache.get(token)
+    user = None
+    if userid:
+        user = User.objects.get(pk=userid)
+
+    return render(request, 'mine/mine.html', context={'user':user})
+
+
+def login(request):
+    return render(request, 'mine/login.html')
+
+
+def logout(request):
+    request.session.flush()
+
+    return redirect('axf:mine')
+
+
+def generate_password(param):
+    md5 = hashlib.md5()
+    md5.update(param.encode('utf-8'))
+    return md5.hexdigest()
+
+
+def generate_token():
+    temp = str(time.time()) + str(random.random())
+    md5 = hashlib.md5()
+    md5.update(temp.encode('utf-8'))
+    return md5.hexdigest()
+
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'mine/register.html')
+    elif request.method == 'POST':
+        # 获取数据
+        email = request.POST.get('email')
+        name = request.POST.get('name')
+        passoword = generate_password(request.POST.get('password'))
+
+        # 存入数据库
+        user = User()
+        user.email = email
+        user.password = passoword
+        user.name = name
+        user.save()
+
+        # 状态保持
+        token = generate_token()
+        # key-value  >>  token:userid
+        cache.set(token, user.id, 60*60*24*3)
+
+        request.session['token'] = token
+
+        return redirect('axf:mine')
